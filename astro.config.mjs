e1e0eb4a9df195635aa3c357d7728735c78a3af7
Fernando Ticona <thefersh24@gmail.com>
2025-08-12 16:15:43 -0400

    feat: :sparkles: add tw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Loproda Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			logo: {
-				src: "./src/assets/logo.png",
-			},
-			credits: false,
-		}),
+  integrations: [
+      starlight({
+          title: 'Loproda Docs',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+          logo: {
+              src: "./src/assets/logo.png",
+          },
+          customCss: [
+            './src/styles/global.css',
+          ],
+          credits: false,
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
