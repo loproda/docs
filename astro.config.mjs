0a2b9bba017ac532b48d80647a1377e419b7346b
Fernando Ticona <thefersh24@gmail.com>
2025-08-12 16:58:45 -0400

    index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,35 +3,62 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 import tailwindcss from '@tailwindcss/vite';
+import { readdir } from "fs/promises";
+
+async function autoGenStyles() {
+	const styles = (
+		await readdir("./src/styles/", {
+			withFileTypes: true,
+			recursive: true,
+		})
+	)
+		.filter((x) => x.isFile())
+		.map((x) => x.parentPath + x.name)
+		.sort((a) => (a === "./src/styles/tailwind.css" ? -1 : 1));
+
+	return styles;
+}
+
+const customCss = await autoGenStyles();
 
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'Loproda Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
-          ],
-          logo: {
-              src: "./src/assets/logo.png",
-          },
-          customCss: [
-            './src/styles/global.css',
-          ],
-          credits: false,
-      }),
+    site: "https://docs.loproda.com",
+	experimental: {
+		contentIntellisense: true,
+	},
+    i18n: {
+        defaultLocale: "es",
+        locales: ["es"]
+    },
+    integrations: [
+        starlight({
+            title: 'Loproda Docs',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/loproda/docs' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
+            logo: {
+                src: "./src/assets/logo.png",
+            },
+            customCss,
+            credits: false,
+            pagination: false,
+            lastUpdated: true,
+            markdown: {
+				headingLinks: false,
+			},
+        }),
 	],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+// Taken from Astro
+export { default as ListCard } from "./astro/ListCard.astro";
+export { default as FourCardGrid } from "./FourCardGrid.astro";
